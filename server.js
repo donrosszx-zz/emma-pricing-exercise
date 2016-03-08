9f487829b99cb8c6c3b16e260b171e803429816e
donrosszx <donrosszx@gmail.com>
2016-03-08 13:43:49 -0600

    started to wire up server and will push to heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
