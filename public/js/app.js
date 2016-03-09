860776d7a7a0f0b78b0d5c17626bc34eec0c0db5
donrosszx <donrosszx@gmail.com>
2016-03-09 10:52:38 -0600

    added spread by region data (unstyled) to back end and front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,18 @@
 var socket = io();
 
-socket.on('analysis', function (analysis) {
+socket.on('analysis', function(analysis) {
     var $overall = jQuery('#overall-cheapest');
     var $item = jQuery('<li></li>');
-    $item.append('<p><strong>' + analysis.least.region + '</strong></p>');
-    $item.append('<p>' + analysis.least.price + '</p>');
+    $item.append('<p><strong>' + analysis.overallLeast.region + '</strong></p>');
+    $item.append('<p>' + analysis.overallLeast.price + '</p>');
     $overall.append($item);
+
+    var $regList = jQuery('#spread-by-region');
+    $.each(analysis.regionSpread, function(i) {
+        var $li = jQuery('<li></li>');
+        $li.append('<p><strong>' + analysis.regionSpread[i].region + '</strong></p>');
+        $li.append('<p><em>Most: </em>' + analysis.regionSpread[i].most.type + '</p>');
+        $li.append('<p><em>Least: </em>' + analysis.regionSpread[i].least.type + '</p>');
+        $regList.append($li);
+    });
 });
--- server.js
+++ server.js
@@ -55,33 +55,50 @@ var spread = function(instances) {
     var mostExpensive = _.sortBy(instances, function(inst) {
         return -inst.price;
     })[0];
+    // returns {most: {}, least: {}}
     return {
         most: mostExpensive,
         least: leastExpensive
     };
 };
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     console.log('user connected via socket.io');
 
     var instances = [];
-    getPricingFromAPI("http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js").then(function (instancesFromAPI) {
+    getPricingFromAPI("http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js").then(function(instancesFromAPI) {
         instances.push.apply(instances, instancesFromAPI);
         return getPricingFromAPI("http://spot-price.s3.amazonaws.com/spot.js");
-    }).then(function (instancesFromAPI) {
+    }).then(function(instancesFromAPI) {
         instances.push.apply(instances, instancesFromAPI);
 
+        // OVERALL CHEAPEST (AND MOST EXPENSIVE)
         var overallSpread = spread(instances);
-        
-        socket.emit('analysis', {
-            most: overallSpread.most,
-            least: overallSpread.least
+
+        // SPREAD BY REGION
+        var regionBasedGroup = _.groupBy(instances, function(inst) {
+            return inst.region;
         });
-    }).catch(function (error) {
+        // we want the format of regionBasedSpread to look like
+        // [{region: 'region1', most: {}, least {}}, {region: 'region2', most: {}, least {}}, etc]
+        var regionBasedSpread = [];
+        _.each(regionBasedGroup, function (value, prop) {
+            // value is a list of region specific instances to perform the spread on
+            // prop is the region name
+            var regionSpread = spread(value);
+            regionBasedSpread.push({region: prop, most: regionSpread.most , least:regionSpread.least});
+        });
+        // console.log(regionBasedSpread);
+
+        socket.emit('analysis', {
+            overallLeast: overallSpread.least,
+            regionSpread: regionBasedSpread
+        });
+    }).catch(function(error) {
         console.log(error);
     });
 });
 
-http.listen(PORT, function () {
+http.listen(PORT, function() {
     console.log('Express server started on port ' + PORT);
 });
